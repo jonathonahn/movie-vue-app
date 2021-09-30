81c70ae1ea284bbb15f0141cc623392fa1996935
jonathon <ahnjon77@gmail.com>
2021-09-30 15:29:51 -0700

    adds the ability to search for a single movie by ID, shows data in a readable format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div>
+      Movie ID: <input type="text" v-model="movieId" />
+      <button v-on:click="showMovie()">Find</button>
+    </div>
+    <div v-if="searchedMovie">
+      Title: {{ searchedMovie.title }}
+      <br />
+      Year: {{ searchedMovie.year }}
+      <br />
+      Director: {{ searchedMovie.director }}
+      <br />
+      Plot: {{ searchedMovie.plot }}
+    </div>
     <div>Title: <input type="text" v-model="newMovieParams.title" /></div>
     <div>Year: <input type="text" v-model="newMovieParams.year" /></div>
     <div>Plot: <input type="text" v-model="newMovieParams.plot" /></div>
     <div>Director: <input type="text" v-model="newMovieParams.director" /></div>
     <button v-on:click="createMovie()">Create Movie</button>
     <div v-for="movie in movies" v-bind:key="movie.id">
-      <p>{{ movie }}</p>
+      <p>{{ movie.title }}</p>
     </div>
   </div>
 </template>
@@ -22,6 +35,8 @@ export default {
       message: "Welcome to Vue.js!",
       movies: [],
       newMovieParams: {},
+      movieId: "",
+      searchedMovie: null,
     };
   },
   created: function () {
@@ -43,6 +58,17 @@ export default {
           this.newMovieParams.year = "";
           this.newMovieParams.plot = "";
           this.newMovieParams.director = "";
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    showMovie: function () {
+      axios
+        .get("http://localhost:3000/movies/" + this.movieId)
+        .then((response) => {
+          this.searchedMovie = response.data;
+          this.movieId = "";
         })
         .catch((error) => {
           console.log(error.response.data.errors);
